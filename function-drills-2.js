d5c576f41c0377e73846dfbab57040a53f4c40ca
Tarik Desire <tprdesire@gmail.com>
2018-07-16 16:35:02 -0400

    Added decode function

--- function-drills-2.js
+++ function-drills-2.js
@@ -5,6 +5,9 @@ function jediName(firstName, lastName) {
   return retString;
 }
 
+jediName('Beyonce', 'Knowles');
+
+
 function beyond(num){
   if (num === Infinity || num === -Infinity) {
     console.log('And beyond');
@@ -16,3 +19,27 @@ function beyond(num){
   console.log('Staying home');
   }
 }
+
+
+
+function decode (string) {
+  let splitStringArr = string.split(' ');
+  let decodeArr = [];
+  splitStringArr.forEach(function(word) {
+    if (word[0] === 'a') {
+      decodeArr.push(word[1]);
+    } else if (word[0] === 'b') {
+      decodeArr.push(word[2]);
+    } else if (word[0] === 'c') {
+      decodeArr.push(word[3]);
+    } else if (word[0] === 'd') {
+      decodeArr.push(word[4]);
+    } else {
+      decodeArr.push(' ');
+    }
+  });
+  let retString = decodeArr.join('');
+  return retString;
+}
+
+decode('craft block argon meter bells brown croon droop');
